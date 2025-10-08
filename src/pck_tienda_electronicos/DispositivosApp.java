7eec2631707e50b947d4689e52330bf930e082df
Diego-Camargo-Oropeza <desarrollo@kint.com.mx>
2025-10-07 19:13:33 -0600

    Avance mínimo de menú y helper para la escritura de archivo

---
+++ src/pck_tienda_electronicos/DispositivosApp.java
@@ -0,0 +1,108 @@
+package pck_tienda_electronicos;
+
+import java.nio.file.Paths;
+import javax.swing.JOptionPane;
+import pck_helper.FileHelper;
+
+public class DispositivosApp {
+
+    public static final String MENU = "1) Alta de un smartphone\n"
+            + "2) Alta de una laptop\n"
+            + "3) Alta de un cliente\n"
+            + "4) Alta de una venta\n"
+            + "5) Listar smartphones\n"
+            + "6) Listar laptops\n"
+            + "7) Listar clientes\n"
+            + "8) Listar ventas\n"
+            + "9) Ver detalle de un smartphone\n"
+            + "10) Ver detalle de una laptop\n"
+            + "11) Ver detalle de un cliente\n"
+            + "12) Ver detalle de una venta\n"
+            + "13) Eliminar un smartphone\n"
+            + "14) Eliminar una laptop\n"
+            + "15) Eliminar un cliente\n"
+            + "16) Eliminar una venta\n"
+            + "17) Salir\n"
+            + "Por favor seleccione una opción\n";
+
+    public static void main(String[] args) {
+        int op;
+        do {
+            do {
+                op = 0;
+                try {
+                    op = Integer.parseInt(JOptionPane.showInputDialog(null, MENU, "Menú de opciones", JOptionPane.QUESTION_MESSAGE));
+                    if (op <= 0 || op > 17) {
+                        JOptionPane.showMessageDialog(null, "Por favor ingrese una opción válida", "Error", JOptionPane.ERROR_MESSAGE);
+                        op = 0;
+                    }
+                } catch (NumberFormatException e) {
+                    JOptionPane.showMessageDialog(null, "Por favor ingrese numéricamente la opción a elegir", "Error", JOptionPane.ERROR_MESSAGE);
+
+                }
+            } while (op == 0);
+
+            switch (op) {
+                case 1 -> {
+
+                }
+                case 1 -> {
+
+                }
+                case 2 -> {
+
+                }
+                case 3 -> {
+
+                }
+                case 4 -> {
+
+                }
+                case 5 -> {
+
+                }
+                case 6 -> {
+
+                }
+                case 7 -> {
+
+                }
+                case 8 -> {
+
+                }
+                case 9 -> {
+
+                }
+                case 10 -> {
+
+                }
+                case 11 -> {
+
+                }
+                case 12 -> {
+
+                }
+                case 13 -> {
+
+                }
+                case 14 -> {
+
+                }
+                case 15 -> {
+
+                }
+                case 16 -> {
+
+                }
+                case 17 -> {
+
+                }
+                default -> {
+
+                }
+            }
+
+        } while (op != 17);
+
+    }
+}
